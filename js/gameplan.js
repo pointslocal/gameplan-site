548d9509674fc90d2e28b92b57c604860535a304
Nathan Kozyra <nkozyra@gmail.com>
2016-01-17 20:20:59 -0500

    minor style changes

--- js/gameplan.js
+++ js/gameplan.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   'use strict';
-  var _winHeight = $( window ).height();
+  var _winHeight = $(window).outerHeight();
   var _headerHeight = $('#android-header').height();
   $('.gameplan-jumbotron').css('min-height', (_winHeight - _headerHeight) + 'px');
   console.log((_winHeight - _headerHeight));
